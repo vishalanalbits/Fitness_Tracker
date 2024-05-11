e8229d7864039840db31ba4b1a2c7c9ac0a4c484
priyanshpandeyBits <2023mt93066@wilp.bits-pilani.ac.in>
2024-05-12 01:36:01 +0530

    Added activity logging and backend Changes
    added activity logging and backend Changes

---
+++ FitnessTracker/ViewModels/ActivityLogViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+namespace FitnessTracker.ViewModels
+{
+  
+    public class ActivityLogViewModel
+    {
+
+        [Required]
+        public string ActivityType { get; set; }
+
+        [Required]
+        [Display(Name = "Duration (minutes)")]
+        public int DurationInMinutes { get; set; }
+
+        [Required]
+        [Display(Name = "Distance (miles/km)")]
+        public double Distance { get; set; }
+
+        [Required]
+        public string Intensity { get; set; }
+
+        [Display(Name = "Calories Burned")]
+        public int CaloriesBurned { get; set; }
+    }
+}
+
+
+
+
